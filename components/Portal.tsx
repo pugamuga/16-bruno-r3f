e97f796b27ebef8e7291e6867b1c7851b9e3598a
Evgeny Pugachev <rabota.pugamuga@gmail.com>
2022-11-05 07:27:56 +0200

    end with portal

--- components/Portal.tsx
+++ components/Portal.tsx
@@ -7,31 +7,45 @@ import {
   useTexture,
 } from "@react-three/drei";
 import { DoubleSide } from "three";
-import portalVertexShader from "../public/portalShaders/vertex"
-import portalFragmentShader from "../public/portalShaders/fragment"
-import * as THREE from "three"
-import { extend } from "@react-three/fiber";
+import portalVertexShader from "../public/portalShaders/vertex";
+import portalFragmentShader from "../public/portalShaders/fragment";
+import * as THREE from "three";
+import { extend, useFrame } from "@react-three/fiber";
+import { useRef } from "react";
 
 const PortalMaterial = shaderMaterial(
-    {uTime:0,
+  {
+    uTime: 0,
     uColorStart: new THREE.Color("#ffffff"),
-    uColorEnd: new THREE.Color("#000000"),},
-    portalVertexShader,
-    portalFragmentShader
+    uColorEnd: new THREE.Color("#000000"),
+  },
+  portalVertexShader,
+  portalFragmentShader
+);
 
-)
-
-extend({PortalMaterial})
-
-
+extend({ PortalMaterial });
 
 export default function Portal(): JSX.Element {
   const portal = useGLTF("./models/portal/portal.glb");
   const bakedTexture = useTexture("./models/portal/baked.jpg");
 
+  const portalMatRef = useRef<any>();
+
+  useFrame((state, delta) => {
+    if (portalMatRef.current) {
+      portalMatRef.current.uTime += delta;
+    }
+  });
+
   return (
     <>
-      <Sparkles size={6} scale={[4, 2, 4]} position={[0, 1, 0]} speed={.2} count={40}/>
+      <Sparkles
+        size={6}
+        scale={[4, 2, 4]}
+        position={[0, 1, 0]}
+        speed={0.2}
+        count={40}
+      />
       <OrbitControls
         minPolarAngle={0}
         maxPolarAngle={Math.PI / 2.1}
@@ -61,9 +75,7 @@ export default function Portal(): JSX.Element {
           rotation={portal.nodes.portalLight.rotation}
           scale={portal.nodes.portalLight.scale}
         >
-          {/* <meshBasicMaterial side={DoubleSide} color={"#ffffe5"}/> */}
-           {/* @ts-ignore */}
-        <portalMaterial/>
+          <portalMaterial ref={portalMatRef} side={DoubleSide} />
         </mesh>
       </Stage>
     </>
