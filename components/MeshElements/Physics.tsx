02ca74482b46f090a654ccc2a5146c8f38769eb4
Evgeny Pugachev <rabota.pugamuga@gmail.com>
2022-11-07 10:36:23 +0200

    react three rapier

--- components/MeshElements/Physics.tsx
+++ components/MeshElements/Physics.tsx
@@ -1,13 +1,25 @@
 import { OrbitControls } from "@react-three/drei";
+import { Perf } from "r3f-perf";
 
 export default function Physics(): JSX.Element {
   return (
     <>
+    <Perf/>
     <color args={["ivory"]} attach="background"/>
       <OrbitControls />
-      <mesh>
+      <directionalLight position={[1,2,3]} castShadow intensity={1.5}/>
+      <ambientLight intensity={.5}/>
+      <mesh castShadow position={[2,2,0]} scale={1.5}>  
         <boxGeometry />
-        <meshNormalMaterial />
+        <meshStandardMaterial color="mediumpurple" />
+      </mesh>
+      <mesh position={[-2,2,0]} castShadow>
+         <sphereGeometry/>
+         <meshStandardMaterial color={'orange'}/>
+      </mesh>
+      <mesh scale={[10,10,.4]} rotation-x={Math.PI*-1/2} receiveShadow >
+         <boxGeometry/>
+         <meshStandardMaterial color={'yellowgreen'}/>
       </mesh>
     </>
   );
